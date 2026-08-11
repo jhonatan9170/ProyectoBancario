24cb735840d43c41b6b033c72a77f3030e279a93
jhonatan9170 <65328388+jhonatan9170@users.noreply.github.com>
2026-08-11 18:25:58 -0500

    add screens

---
+++ ProyectoBancario/Views/ForYou/ForYouViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ForYouViewController.swift
+//  ProyectoBancario
+//
+//  Created by Jhonatan Chavez on 11/08/26.
+//
+
+import UIKit
+
+class ForYouViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+}
---
+++ ProyectoBancario/Views/MyAccount/MyAccountViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyAccountViewController.swift
+//  ProyectoBancario
+//
+//  Created by Jhonatan Chavez on 11/08/26.
+//
+
+import UIKit
+
+class MyAccountViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+    
+}
